36077b41ac60b379dd7b5c60d34c3f61b98743f0
Marco Ferrufino <cpanelmarco@yahoo.com>
2013-08-30 17:34:31 -0500

    corrected link

--- backup_report.sh
+++ backup_report.sh
@@ -4,7 +4,7 @@
 # Description: https://staffwiki.cpanel.net/bin/view/LinuxSupport/CPanelBackups
 #
 # How to run this script:
-# curl -s --insecure https://raw.github.com/cPMarco/cpm/master/backup_report.sh | sh
+# curl -s --insecure https://raw.github.com/cPanelTechs/TechScripts/master/backup_report.sh | sh
 
 # this shows backups enabled or disabled but i need to return the value to the check functions
 
